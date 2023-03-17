30fa01e14d5790f1603537a77e11874e2764299e
Víctor Falcón <victoor89@gmail.com>
2023-03-17 17:19:49 +0100

    Listen to tally webhooks

---
+++ app/Http/Controllers/Tally/PostTallyController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Tally;
+
+use App\Http\Controllers\Controller;
+use App\Models\Lead;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class PostTallyController extends Controller
+{
+    public function __invoke(Request $request): Response
+    {
+        $leadUuid = collect($request->json('data.fields', []))
+            ->find(fn($field) => $field['label'] === 'lead_id');
+
+        Lead::where('uuid', $leadUuid['value'])
+            ->update([
+                'form_filled' => true,
+            ]);
+
+        return response()->noContent();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Checkout\GetCheckoutController;
 use App\Http\Controllers\Lead\GetLeadController;
 use App\Http\Controllers\Lead\PostLeadController;
+use App\Http\Controllers\Tally\PostTallyController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,9 +21,14 @@ Route::view('/', 'welcome');
 Route::view('/privacy-policy', 'privacy-policy');
 Route::view('/terms-and-conditions', 'terms-and-conditions');
 
+// Leads
 Route::get('/lead/{lead:uuid}', GetLeadController::class);
 Route::post('/lead', PostLeadController::class);
 
+// Prices
 Route::get('/lead/{lead:uuid}/price/{price}', GetCheckoutController::class);
+
+// Tally
+Route::post('/tally', PostTallyController::class);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
