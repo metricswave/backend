73a132c9b1e30033b95975d6ff4d1f8fab01d554
Víctor Falcón <victoor89@gmail.com>
2023-03-18 12:33:41 +0000

    Add optional mail param to app:mail:user-lifetime-license-mail

--- app/Console/Commands/UserLifetimeLicenceMailCommand.php
+++ app/Console/Commands/UserLifetimeLicenceMailCommand.php
@@ -12,20 +12,24 @@ class UserLifetimeLicenceMailCommand extends Command
 {
     private const MAIL_TYPE = 'user-lifetime-license-v2';
 
-    protected $signature = 'app:mail:user-lifetime-licence-mail';
+    protected $signature = 'app:mail:user-lifetime-licence-mail {email?}';
 
     protected $description = 'Send lifetime license deal to user who has not bought a license yet.';
 
     public function handle(): void
     {
-        $leads = Lead::whereNull('paid_at')->get();
-        $mailLogs = MailLog::where('type', self::MAIL_TYPE)->get()->pluck('mail');
+        if ($email = $this->argument('email')) {
+            $leads = Lead::where('email', $email)->get();
+        } else {
+            $leads = Lead::whereNull('paid_at')->get();
+            $mailLogs = MailLog::where('type', self::MAIL_TYPE)->get()->pluck('mail');
 
-        $leads = $leads->filter(function (Lead $lead) use ($mailLogs) {
-            return !$mailLogs->contains($lead->email);
-        });
+            $leads = $leads->filter(function (Lead $lead) use ($mailLogs) {
+                return !$mailLogs->contains($lead->email);
+            });
+        }
 
-        $this->withProgressBar($leads, function (Lead $lead) use ($mailLogs) {
+        $this->withProgressBar($leads, function (Lead $lead) {
             Mail::send(new LifetimeLicenseDealMail($lead));
 
             MailLog::create([
