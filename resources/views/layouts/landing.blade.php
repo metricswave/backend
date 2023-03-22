2835b2735027ba75aefddde782c2dc2098cd3069
Víctor Falcón <victoor89@gmail.com>
2023-03-22 10:11:54 +0100

    Add OG tags

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -9,6 +9,31 @@
     <meta content="Real-time notifications for everything that matters to you."
           name="description"/>
 
+    {{-- OG Tags --}}
+    <meta property="og:url"
+          content="{{ config('app.url') }}"/>
+    <meta property="og:locale"
+          content="en_US"/>
+    <meta property="og:type"
+          content="article"/>
+    <meta property="og:title"
+          content="{{ config('app.name') }}"/>
+    <meta property="og:description"
+          content="Real-time notifications for everything that matters to you."/>
+    <meta property="og:image"
+          content="/images/notifywave.png"/>
+
+    <meta name="twitter:card"
+          content="summary_large_image"/>
+    <meta name="twitter:title"
+          content="{{ config('app.name') }}"/>
+    <meta name="twitter:description"
+          content="Real-time notifications for everything that matters to you."/>
+    <meta name="twitter:creator"
+          content="@get_monse"/>
+    <meta name="twitter:image"
+          content="/images/notifywave.png"/>
+
     {{-- Favicon --}}
     <link rel="apple-touch-icon"
           sizes="57x57"
