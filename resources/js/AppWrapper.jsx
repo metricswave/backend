0733f10576e51b7ef5664f5108d2440627beebc0
Víctor Falcón <victoor89@gmail.com>
2023-03-28 10:02:31 +0200

    Use new CreateRoot method from react

--- resources/js/AppWrapper.jsx
+++ resources/js/AppWrapper.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
 
-export default function AppWrapper() {
+function AppWrapper({callback}) {
     return (
-        <h1>Hello from React</h1>
+        <div ref={callback}>
+            <h1>Hello World</h1>
+        </div>
     );
 }
 
-if (document.getElementById('app')) {
-    ReactDOM.render(<AppWrapper/>, document.getElementById('app'));
-}
+const rootElement = document.getElementById("app");
+
+const root = ReactDOMClient.createRoot(rootElement);
+root.render(<AppWrapper callback={() => {
+    // This callback is called after the component is mounted
+}}/>);
