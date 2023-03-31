424ddff0e3bdb9f882299e31e45d6f9a11967dc3
Víctor Falcón <victoor89@gmail.com>
2023-03-31 14:23:29 +0100

    Mail article summaries instead of full text

--- resources/views/mail/blog_article.blade.php
+++ resources/views/mail/blog_article.blade.php
@@ -1,4 +1,5 @@
 <x-mail::message>
 ## {{ $article->title }}
-{!! $article->content !!}
+{!! $article->short_content !!}
+<p><a href="{{ config('app.url') }}/blog/{{ $article->slug }}">Read more →</a></p>
 </x-mail::message>
