82538b4ac3cc23e2eecbd3799eedc1de622166d6
Daniel Falcón Ruiz <dani-basquetball@hotmail.es>
2023-04-02 15:56:34 +0100

    Use ApiAuthJsonController and fix tests

--- app/Http/Controllers/Api/Auth/LogoutController.php
+++ app/Http/Controllers/Api/Auth/LogoutController.php
@@ -2,23 +2,15 @@
 
 namespace App\Http\Controllers\Api\Auth;
 
-use App\Http\Controllers\Api\JsonController;
+use App\Http\Controllers\Api\ApiAuthJsonController;
 use App\Http\Requests\LogoutRequest;
-use App\Models\TokenAbility;
 use Illuminate\Http\JsonResponse;
 
-class LogoutController extends JsonController
+class LogoutController extends ApiAuthJsonController
 {
-
-    public function __construct()
-    {
-        $this->middleware(['auth:sanctum', 'abilities:' . TokenAbility::API]);
-    }
-
     public function __invoke(LogoutRequest $request): JsonResponse
     {
         $this->user()->tokens()->where('name', $request->device_name)->delete();
         return $this->response(["message" => 'User logged out successfully.']);
     }
-
 }
--- tests/Feature/Http/Controllers/Api/Auth/LogoutControllerTest.php
+++ tests/Feature/Http/Controllers/Api/Auth/LogoutControllerTest.php
@@ -4,19 +4,25 @@ use App\Models\User;
 
 it('successfully logs out a user', function () {
     $user = User::factory()->create(['password' => Hash::make('password')]);
-    $tokens = $user->createTokens('test');
 
     $this
+        ->actingAs($user)
         ->postJson(
             '/api/logout',
             [
                 'device_name' => 'test',
             ],
-            [
-                'Authorization' => 'Bearer ' . $tokens['refresh_token']['token'],
-            ]
         )
         ->assertSuccessful();
 
     $this->assertCount(0, $user->tokens);
 });
+
+it('return error if no auth provided')
+    ->postJson(
+        '/api/logout',
+        [
+            'device_name' => 'test',
+        ],
+    )
+    ->assertUnauthorized();
