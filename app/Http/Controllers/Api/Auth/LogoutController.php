13d7f7f7e8acb6b1e76fc5b7ebc13662acf9a63c
danifalcon <37447896+danifalcon@users.noreply.github.com>
2023-04-02 16:00:09 +0100

    Implement logout endpoint (#3)

---
+++ app/Http/Controllers/Api/Auth/LogoutController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Api\ApiAuthJsonController;
+use App\Http\Requests\LogoutRequest;
+use Illuminate\Http\JsonResponse;
+
+class LogoutController extends ApiAuthJsonController
+{
+    public function __invoke(LogoutRequest $request): JsonResponse
+    {
+        $this->user()->tokens()->where('name', $request->device_name)->delete();
+        return $this->response(["message" => 'User logged out successfully.']);
+    }
+}
---
+++ tests/Feature/Http/Controllers/Api/Auth/LogoutControllerTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Models\User;
+
+it('successfully logs out a user', function () {
+    $user = User::factory()->create(['password' => Hash::make('password')]);
+
+    $this
+        ->actingAs($user)
+        ->postJson(
+            '/api/logout',
+            [
+                'device_name' => 'test',
+            ],
+        )
+        ->assertSuccessful();
+
+    $this->assertCount(0, $user->tokens);
+});
+
+it('return error if no auth provided')
+    ->postJson(
+        '/api/logout',
+        [
+            'device_name' => 'test',
+        ],
+    )
+    ->assertUnauthorized();
