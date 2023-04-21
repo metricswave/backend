a91f194ac7b88ca69d704fe55d01a85afd725f38
Víctor Falcón <victoor89@gmail.com>
2023-04-21 22:58:21 +0200

    Notify new leads via NotifyWave

--- app/Services/Leads/FirstOrCreateLeadService.php
+++ app/Services/Leads/FirstOrCreateLeadService.php
@@ -3,15 +3,34 @@
 namespace App\Services\Leads;
 
 use App\Models\Lead;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Str;
 
 class FirstOrCreateLeadService
 {
     public function __invoke(string $mail): Lead
     {
-        return Lead::firstOrCreate(
-            ['email' => $mail],
-            ['uuid' => Str::random(15)]
+        if (Lead::query()->where('email', $mail)->exists()) {
+            return Lead::query()->where('email', $mail)->first();
+        }
+
+        $lead = Lead::create([
+            'email' => $mail,
+            'uuid' => Str::random(15),
+        ]);
+
+        $this->notify();
+
+        return $lead;
+    }
+
+    private function notify(): void
+    {
+        Http::get(
+            'https://notifywave.com/webhooks/29c31fab-a1c6-491d-92ff-081e69744651',
+            [
+                'count' => Lead::query()->count(),
+            ]
         );
     }
 }
