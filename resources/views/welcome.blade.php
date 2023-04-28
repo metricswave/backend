69d04e9550b37d1456c8915d49ca216205ba66ce
Víctor Falcón <victoor89@gmail.com>
2023-04-28 16:20:40 +0200

    Add warn in the homepage about the beta date

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="flex flex-col items-center justify-center space-y-14 sm:space-y-32">
-        <div class="mw-landing mx-auto my-20 flex flex-col space-y-8 sm:my-40">
+        <div class="mw-landing mx-auto my-10 flex flex-col space-y-8 sm:my-40">
             <h1 class="text-3xl sm:text-6xl leading-[1.3] sm:leading-[1.1] tracking-tighter">
                 <span class="relative bg-gradient-to-br from-pink-500 to-amber-500 bg-clip-text text-transparent">Notifications</span>
                 <br/>for everything that matters to you
@@ -17,6 +17,27 @@
             <p class="text-lg font-light leading-normal sm:text-2xl">Never miss a beat and stay on top of your
                 game.</p>
             @include('landing.lead-form')
+
+            @if(Date::createFromFormat('Y-m-d', '2023-05-03')->isFuture())
+                <a href="/blog/on-may-3rd-we-will-open-the-beta-to-the-first-users"
+                   class="w-full flex items-center rounded-full block bg-yellow-50 py-3 px-5">
+                    <div class="mr-4">
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                             fill="currentColor"
+                             width="20px"
+                             height="20px"
+                             viewBox="0 0 32 32"
+                             version="1.1">
+                            <path d="M18 23l-1-0v-8.938c0-0.011-0.003-0.021-0.003-0.031s0.003-0.020 0.003-0.031c0-0.552-0.448-1-1-1h-2c-0.552 0-1 0.448-1 1s0.448 1 1 1h1v8h-1c-0.552 0-1 0.448-1 1s0.448 1 1 1h4c0.552 0 1-0.448 1-1s-0.448-1-1-1zM16 11c1.105 0 2-0.896 2-2s-0.895-2-2-2-2 0.896-2 2 0.896 2 2 2zM16-0c-8.836 0-16 7.163-16 16s7.163 16 16 16c8.837 0 16-7.163 16-16s-7.163-16-16-16zM16 30.031c-7.72 0-14-6.312-14-14.032s6.28-14 14-14 14 6.28 14 14-6.28 14.032-14 14.032z"/>
+                        </svg>
+                    </div>
+
+                    <div>
+                        <span>First beta access on May 3rd.</span>
+                        <span class="text-blue-500 underline hover:no-underline smooth">Read more →</span>
+                    </div>
+                </a>
+            @endif
         </div>
 
         <div class="relative w-full">
