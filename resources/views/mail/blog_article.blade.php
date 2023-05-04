11b2ce56ff350148e6384686a3feeba0b44b867b
Víctor Falcón <victoor89@gmail.com>
2023-05-04 09:26:56 +0100

    Recover absoluteUrl() method to get article link in blog article mail

--- resources/views/mail/blog_article.blade.php
+++ resources/views/mail/blog_article.blade.php
@@ -1,5 +1,5 @@
 <x-mail::message>
 ## {{ $article->title }}
 {!! $article->short_content !!}
-<p><a href="{{ config('app.url') }}/blog/{{ $article->slug }}">Read more →</a></p>
+<p><a href="{{ $article->absoluteUrl() }}">Read more →</a></p>
 </x-mail::message>
