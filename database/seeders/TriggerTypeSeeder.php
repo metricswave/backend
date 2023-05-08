48f5d8af1ece198defcf376dde6dc7d4976c988e
Víctor Falcón <victoor89@gmail.com>
2023-05-08 10:17:41 +0100

    Update TriggerTypeSeeder to remove beta from Daily Weather summary and add TimeToLive (in local only)

--- database/seeders/TriggerTypeSeeder.php
+++ database/seeders/TriggerTypeSeeder.php
@@ -70,7 +70,7 @@ class TriggerTypeSeeder extends Seeder
         TriggerType::updateOrCreate(
             ['id' => TriggerTypeId::WeatherSummary],
             [
-                'name' => 'Daily Weather Summary (Beta)',
+                'name' => 'Daily Weather Summary',
                 'icon' => 'weather.png',
                 'description' => 'Receive a summary of the weather for a location.',
                 'configuration' => [
@@ -86,6 +86,73 @@ class TriggerTypeSeeder extends Seeder
                             'name' => 'time',
                             'type' => 'time',
                             'label' => 'Time',
+                            'required' => true,
+                            'default' => '08:00',
+                        ],
+                        [
+                            'name' => 'weekdays',
+                            'type' => 'weekdays',
+                            'label' => 'Weekdays',
+                            'required' => true,
+                            'multiple' => true,
+                            'default' => [
+                                'monday',
+                                'tuesday',
+                                'wednesday',
+                                'thursday',
+                                'friday',
+                                'saturday',
+                                'sunday',
+                            ],
+                        ]
+                    ],
+                ],
+            ],
+        );
+
+        if (app()->environment('production')) {
+            return;
+        }
+
+        TriggerType::updateOrCreate(
+            ['id' => TriggerTypeId::TimeToLeave],
+            [
+                'name' => 'Time to Leave (Beta)',
+                'icon' => 'live_transit.png',
+                'description' => 'Receive a notification when it\'s time to leave based on live transit data.',
+                'configuration' => [
+                    'version' => '1.0',
+                    'fields' => [
+                        [
+                            'name' => 'origin',
+                            'type' => 'address',
+                            'label' => 'Origin',
+                            'required' => true,
+                        ],
+                        [
+                            'name' => 'destination',
+                            'type' => 'address',
+                            'label' => 'Destination',
+                            'required' => true,
+                        ],
+                        [
+                            'name' => 'mode',
+                            'type' => 'select',
+                            'label' => 'Mode',
+                            'required' => true,
+                            'multiple' => false,
+                            'default' => 'driving',
+                            'options' => [
+                                ['value' => 'driving', 'label' => 'Driving'],
+                                ['value' => 'walking', 'label' => 'Walking'],
+                                ['value' => 'bicycling', 'label' => 'Bicycling'],
+                                ['value' => 'transit', 'label' => 'Transit'],
+                            ]
+                        ],
+                        [
+                            'name' => 'arrival_time',
+                            'type' => 'time',
+                            'label' => 'Arrival Time',
                             'required' => true,
                             'default' => '08:00',
                         ],
