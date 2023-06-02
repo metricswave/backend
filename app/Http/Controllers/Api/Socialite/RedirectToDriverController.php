922d2b56fb25800a199f6347d8e0b408fe841f0d
Víctor Falcón <victoor89@gmail.com>
2023-06-02 12:43:36 +0200

    Connect Google Service with offline access_type

--- app/Http/Controllers/Api/Socialite/RedirectToDriverController.php
+++ app/Http/Controllers/Api/Socialite/RedirectToDriverController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\Socialite;
 
 use App\Http\Controllers\Api\JsonController;
 use App\Models\Service;
+use App\Transfers\ServiceId;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Socialite;
@@ -15,11 +16,16 @@ class RedirectToDriverController extends JsonController
         $creating = null === request()->header('Authorization');
         $scopes = $service->scopesFor($creating);
 
-        /** @var RedirectResponse $path */
-        $path = Socialite::driver($service->driver)
+        $driver = Socialite::driver($service->driver)
             ->stateless()
-            ->scopes($scopes)
-            ->redirect();
+            ->scopes($scopes);
+
+        if ($service->id === ServiceId::Google->value) {
+            $driver->with(['access_type' => 'offline', 'prompt' => 'consent select_account']);
+        }
+
+        /** @var RedirectResponse $path */
+        $path = $driver->redirect();
 
         return $this->response([
             'path' => $path->getTargetUrl(),
