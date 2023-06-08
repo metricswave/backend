49200802f70b8e47f496f2bf4191327282fcabd3
Víctor Falcón <victoor89@gmail.com>
2023-06-08 13:01:23 +0200

    Track visits with NotifyWave

---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+// Make a get request to the next URL with current path if not localhost
+// https://notifywave.com/webhooks/fbe17995-b16b-45d5-b33e-7a43b9a41313?path={value}
+// Example: https://notifywave.com/webhooks/fbe17995-b16b-45d5-b33e-7a43b9a41313?path=/home
+
+if (window.location.hostname !== 'localhost' && window.location.hostname !== 'notifywave.test') {
+    fetch('https://notifywave.com/webhooks/fbe17995-b16b-45d5-b33e-7a43b9a41313?path=' + window.location.pathname)
+} else {
+    console.log('Tracking visit', {path: window.location.pathname})
+}
