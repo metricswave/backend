267c24c773ff159f1e9fe67537ad68b690bcb353
Víctor Falcón <victoor89@gmail.com>
2023-06-11 17:58:15 +0200

    Add more info to landing visits

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,14 @@
 // Example: https://notifywave.com/webhooks/fbe17995-b16b-45d5-b33e-7a43b9a41313?path=/home
 
 if (window.location.hostname !== 'localhost' && window.location.hostname !== 'notifywave.test') {
-    fetch('https://notifywave.com/webhooks/fbe17995-b16b-45d5-b33e-7a43b9a41313?path=' + window.location.pathname)
+    fetch(
+        'https://notifywave.com/webhooks/fbe17995-b16b-45d5-b33e-7a43b9a41313?' +
+        'path=' + window.location.pathname +
+        '&language=' + window.navigator.language +
+        '&userAgent=' + window.navigator.userAgent +
+        '&platform=' + window.navigator.platform +
+        '&referrer=' + document.referrer
+    )
 } else {
     console.log('Tracking visit', {path: window.location.pathname})
 }
