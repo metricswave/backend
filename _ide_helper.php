d25a4a92e4a7b70c12593d6d4c72879882255ce1
Víctor Falcón <victoor89@gmail.com>
2023-06-12 12:49:08 +0200

    Update Statamic

--- _ide_helper.php
+++ _ide_helper.php
@@ -4283,6 +4283,28 @@
                         return \Illuminate\Cache\Repository::rememberWithExpiration($cacheKey, $callback);
         }
                     /**
+         * Remove all items from the cache.
+         *
+         * @return bool 
+         * @static 
+         */ 
+        public static function flush()
+        {
+                        /** @var \Illuminate\Cache\ArrayStore $instance */
+                        return $instance->flush();
+        }
+                    /**
+         * Get the cache key prefix.
+         *
+         * @return string 
+         * @static 
+         */ 
+        public static function getPrefix()
+        {
+                        /** @var \Illuminate\Cache\ArrayStore $instance */
+                        return $instance->getPrefix();
+        }
+                    /**
          * Get a lock instance.
          *
          * @param string $name
@@ -4292,8 +4314,8 @@
          * @static 
          */ 
         public static function lock($name, $seconds = 0, $owner = null)
-        {            //Method inherited from \Illuminate\Cache\FileStore         
-                        /** @var \Statamic\Extensions\FileStore $instance */
+        {
+                        /** @var \Illuminate\Cache\ArrayStore $instance */
                         return $instance->lock($name, $seconds, $owner);
         }
                     /**
@@ -4305,53 +4327,9 @@
          * @static 
          */ 
         public static function restoreLock($name, $owner)
-        {            //Method inherited from \Illuminate\Cache\FileStore         
-                        /** @var \Statamic\Extensions\FileStore $instance */
+        {
+                        /** @var \Illuminate\Cache\ArrayStore $instance */
                         return $instance->restoreLock($name, $owner);
-        }
-                    /**
-         * Remove all items from the cache.
-         *
-         * @return bool 
-         * @static 
-         */ 
-        public static function flush()
-        {            //Method inherited from \Illuminate\Cache\FileStore         
-                        /** @var \Statamic\Extensions\FileStore $instance */
-                        return $instance->flush();
-        }
-                    /**
-         * Get the Filesystem instance.
-         *
-         * @return \Illuminate\Filesystem\Filesystem 
-         * @static 
-         */ 
-        public static function getFilesystem()
-        {            //Method inherited from \Illuminate\Cache\FileStore         
-                        /** @var \Statamic\Extensions\FileStore $instance */
-                        return $instance->getFilesystem();
-        }
-                    /**
-         * Get the working directory of the cache.
-         *
-         * @return string 
-         * @static 
-         */ 
-        public static function getDirectory()
-        {            //Method inherited from \Illuminate\Cache\FileStore         
-                        /** @var \Statamic\Extensions\FileStore $instance */
-                        return $instance->getDirectory();
-        }
-                    /**
-         * Get the cache key prefix.
-         *
-         * @return string 
-         * @static 
-         */ 
-        public static function getPrefix()
-        {            //Method inherited from \Illuminate\Cache\FileStore         
-                        /** @var \Statamic\Extensions\FileStore $instance */
-                        return $instance->getPrefix();
         }
          
     }
@@ -19208,6 +19186,89 @@
      
 }
 
+    namespace Torann\GeoIP\Facades { 
+            /**
+     * 
+     *
+     */ 
+        class GeoIP {
+                    /**
+         * Get the location from the provided IP.
+         *
+         * @param string $ip
+         * @return \Torann\GeoIP\Location 
+         * @throws \Exception
+         * @static 
+         */ 
+        public static function getLocation($ip = null)
+        {
+                        /** @var \Torann\GeoIP\GeoIP $instance */
+                        return $instance->getLocation($ip);
+        }
+                    /**
+         * Get the currency code from ISO.
+         *
+         * @param string $iso
+         * @return string 
+         * @static 
+         */ 
+        public static function getCurrency($iso)
+        {
+                        /** @var \Torann\GeoIP\GeoIP $instance */
+                        return $instance->getCurrency($iso);
+        }
+                    /**
+         * Get service instance.
+         *
+         * @return \Torann\GeoIP\Contracts\ServiceInterface 
+         * @throws Exception
+         * @static 
+         */ 
+        public static function getService()
+        {
+                        /** @var \Torann\GeoIP\GeoIP $instance */
+                        return $instance->getService();
+        }
+                    /**
+         * Get cache instance.
+         *
+         * @return \Torann\GeoIP\Cache 
+         * @static 
+         */ 
+        public static function getCache()
+        {
+                        /** @var \Torann\GeoIP\GeoIP $instance */
+                        return $instance->getCache();
+        }
+                    /**
+         * Get the client IP address.
+         *
+         * @return string 
+         * @static 
+         */ 
+        public static function getClientIP()
+        {
+                        /** @var \Torann\GeoIP\GeoIP $instance */
+                        return $instance->getClientIP();
+        }
+                    /**
+         * Get configuration value.
+         *
+         * @param string $key
+         * @param mixed $default
+         * @return mixed 
+         * @static 
+         */ 
+        public static function config($key, $default = null)
+        {
+                        /** @var \Torann\GeoIP\GeoIP $instance */
+                        return $instance->config($key, $default);
+        }
+         
+    }
+     
+}
+
     namespace Wilderborn\Partyline { 
             /**
      * 
@@ -19477,6 +19538,13 @@
         {
                         return \Illuminate\Cache\Repository::rememberWithExpiration($cacheKey, $callback);
         }
+         
+    }
+            /**
+     * 
+     *
+     */ 
+        class TaggedCache {
          
     }
      
@@ -23337,6 +23405,7 @@ namespace  {
             class GraphQL extends \Rebing\GraphQL\Support\Facades\GraphQL {}
             class Flare extends \Spatie\LaravelIgnition\Facades\Flare {}
             class Statamic extends \Statamic\Statamic {}
+            class GeoIP extends \Torann\GeoIP\Facades\GeoIP {}
             class Partyline extends \Wilderborn\Partyline\Facade {}
      
 }
