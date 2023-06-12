3f6d72f64ec53f553c9a0302b65eb736098414d2
Víctor Falcón <victoor89@gmail.com>
2023-06-12 11:02:28 +0200

    Redirect all notifywave.com to metricswave.com except webhooks

--- routes/web.php
+++ routes/web.php
@@ -18,38 +18,38 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 */
 
-Route::view('/', 'welcome');
 Route::middleware(RedirectOldNotifyWaveDomain::class)->group(function () {
+    Route::view('/', 'welcome');
     Route::permanentRedirect('/roadmap', '/');
+    Route::get('/blog', BlogController::class);
+    Route::get('/blog/category/{slug}', CategoryController::class);
+
+    // Use case landings
+    Route::permanentRedirect('/trigger/monitoring', '/');
+    Route::permanentRedirect('/trigger/calendar-time-to-leave', '/');
+    Route::permanentRedirect('/trigger/deployments-notification', '/');
+    Route::permanentRedirect('/trigger/terminal', '/');
+    Route::permanentRedirect('/trigger/medication-reminder', '/');
+    Route::permanentRedirect('/trigger/weather-summary', '/');
+    Route::permanentRedirect('/trigger/time-to-leave', '/');
+
+
+    Route::view('/privacy-policy', 'privacy-policy');
+    Route::view('/terms-and-conditions', 'terms-and-conditions');
+
+    // Open page metrics
+    Route::get('/open', GetOpenPageController::class);
+
+    // Leads
+    Route::view('/leads', 'leads');
+    Route::get('/leads/{lead:uuid}', GetLeadController::class);
+    Route::post('/leads', PostLeadController::class);
+
+    // Prices
+    Route::get('/leads/create/prices/{price}', GetCheckoutCreatingLeadController::class);
+    Route::get('/leads/{lead:uuid}/prices/{price}', GetCheckoutController::class);
+    Route::view('/payment/success', 'payment.success');
 });
-Route::get('/blog', BlogController::class);
-Route::get('/blog/category/{slug}', CategoryController::class);
-
-// Use case landings
-Route::permanentRedirect('/trigger/monitoring', '/');
-Route::permanentRedirect('/trigger/calendar-time-to-leave', '/');
-Route::permanentRedirect('/trigger/deployments-notification', '/');
-Route::permanentRedirect('/trigger/terminal', '/');
-Route::permanentRedirect('/trigger/medication-reminder', '/');
-Route::permanentRedirect('/trigger/weather-summary', '/');
-Route::permanentRedirect('/trigger/time-to-leave', '/');
-
-
-Route::view('/privacy-policy', 'privacy-policy');
-Route::view('/terms-and-conditions', 'terms-and-conditions');
-
-// Open page metrics
-Route::get('/open', GetOpenPageController::class);
-
-// Leads
-Route::view('/leads', 'leads');
-Route::get('/leads/{lead:uuid}', GetLeadController::class);
-Route::post('/leads', PostLeadController::class);
-
-// Prices
-Route::get('/leads/create/prices/{price}', GetCheckoutCreatingLeadController::class);
-Route::get('/leads/{lead:uuid}/prices/{price}', GetCheckoutController::class);
-Route::view('/payment/success', 'payment.success');
 
 // Webhooks notification
 Route::get('/webhooks/{trigger:uuid}', GetWebhookTriggerController::class);
