e8c65a9854372b7e183f1ddbd9606bf238d11fb4
Víctor Falcón <victoor89@gmail.com>
2023-06-13 11:59:36 +0200

    New script to track visits

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,33 +1,16 @@
-// Make a get request to the next URL with current path if not localhost
-// https://notifywave.com/webhooks/fbe17995-b16b-45d5-b33e-7a43b9a41313?path={value}
-// Example: https://notifywave.com/webhooks/fbe17995-b16b-45d5-b33e-7a43b9a41313?path=/home
-
-if (window.location.hostname !== 'localhost' && window.location.hostname !== 'notifywave.test') {
-    fetch(
-        'https://notifywave.com/webhooks/fbe17995-b16b-45d5-b33e-7a43b9a41313?' +
-        'path=' + window.location.pathname +
-        '&language=' + window.navigator.language +
-        '&userAgent=' + window.navigator.userAgent +
-        '&platform=' + window.navigator.platform +
-        '&referrer=' + document.referrer
-    )
-} else {
-    console.log('Tracking visit', {path: window.location.pathname})
-}
-
 const urlRef = (new URLSearchParams(window.location.search)).get("utm_source")
 if (urlRef !== null) {
-    localStorage.setItem("nw:referrer", urlRef)
+    localStorage.setItem("metricswave:referrer", urlRef)
 } else if (
     document.referrer
     && document.referrer !== window.location.hostname
-    && localStorage.getItem("nw:referrer") === null
+    && localStorage.getItem("metricswave:referrer") === null
 ) {
-    localStorage.setItem("nw:referrer", document.referrer)
+    localStorage.setItem("metricswave:referrer", document.referrer)
 }
 
 const goToApp = () => {
-    window.location.href = 'https://app.metricswave.com?utm_source=' + localStorage.getItem("nw:referrer")
+    window.location.href = 'https://app.metricswave.com?utm_source=' + localStorage.getItem("metricswave:referrer")
 }
 
 document.querySelectorAll('.linkToApp').forEach((el) => {
