f8d2120691030cdfa3ad8fd4be8f331f78b43511
Víctor Falcón <victoor89@gmail.com>
2023-06-23 17:43:04 +0200

    Open Stats: yearly count should be an estimation
    We need to take into account that the first day with stats was May 5th, 2023
    and the current day number of the year.

--- app/Http/Controllers/Open/GetOpenPageController.php
+++ app/Http/Controllers/Open/GetOpenPageController.php
@@ -16,18 +16,25 @@ class GetOpenPageController extends Controller
 
     public function __invoke(): View|Factory
     {
-        $monthlyCount = visitsService(User::class, User::TRIGGER_NOTIFICATION)->period('month')->count();
+        $firstDayWithStats = now()->year === 2023 ? 126 : 0;
+        $yearlyEstimation = (
+            visitsService(User::class, User::TRIGGER_NOTIFICATION)->period('year')->count()
+            / (now()->dayOfYear - $firstDayWithStats)
+            * 365
+        );
+
         return view('open.open', [
-            // 'leadsCount' => Lead::count(),
-            // 'triggersCount' => Trigger::count(),
             'notifications' => [
                 'weekly' => $this->formatLongNumbers(
                     visitsService(User::class, User::TRIGGER_NOTIFICATION)->period('week')->count()
                 ),
-                'monthly' => $this->formatLongNumbers($monthlyCount),
-                'yearly' => $this->formatLongNumbers($monthlyCount * 12),
+                'monthly' => $this->formatLongNumbers(
+                    visitsService(User::class, User::TRIGGER_NOTIFICATION)->period('month')->count()
+                ),
+                'yearly' => $this->formatLongNumbers(
+                    $yearlyEstimation
+                ),
             ],
-            // 'income' => Lead::sum('paid_price'),
         ]);
     }
 
