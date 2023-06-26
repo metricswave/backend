60faf6aeb2a44538cf8b0affdfbb9ca3a7db2342
Víctor Falcón <victoor89@gmail.com>
2023-06-26 07:06:10 +0200

    Trigger Limit Reached notification

--- app/Listeners/CheckUsageLimitOnTriggerNotificationSent.php
+++ app/Listeners/CheckUsageLimitOnTriggerNotificationSent.php
@@ -5,6 +5,7 @@ namespace App\Listeners;
 use App\Events\TriggerNotificationSent;
 use App\Notifications\TriggerLimitReachedNotification;
 use App\Services\CacheKey;
+use Http;
 use Illuminate\Support\Facades\Cache;
 
 class CheckUsageLimitOnTriggerNotificationSent
@@ -15,6 +16,9 @@ class CheckUsageLimitOnTriggerNotificationSent
         $key = CacheKey::generateForModel($user, 'trigger_notification_sent');
 
         if ($user->triggerNotificationVisitsLimitReached() && !Cache::has($key)) {
+            Http::post('https://metricswave.com/webhooks/d5c2d8ab-983e-4653-8e92-b6dc4c55ee6a', [
+                'email' => $user->email,
+            ]);
             $user->notify(new TriggerLimitReachedNotification());
             Cache::put($key, "1", now()->addDays(3));
         }
