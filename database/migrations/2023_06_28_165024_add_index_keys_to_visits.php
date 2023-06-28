bec9151e647bd29328a3892c3d363f286c8a8495
Víctor Falcón <victoor89@gmail.com>
2023-06-28 18:51:41 +0200

    Add migration to visits table

---
+++ database/migrations/2023_06_28_165024_add_index_keys_to_visits.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('visits', function (Blueprint $table) {
+            $table->index(['primary_key', 'expired_at']);
+            $table->index(['primary_key']);
+            $table->index(['expired_at']);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('visits', function (Blueprint $table) {
+            $table->dropIndex(['primary_key', 'expired_at']);
+            $table->dropIndex(['primary_key']);
+            $table->dropIndex(['expired_at']);
+        });
+    }
+};
