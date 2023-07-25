709a34097261b34974714837b1eec9aec7925fa9
Víctor Falcón <victoor89@gmail.com>
2023-07-25 17:37:24 +0200

    Fix header padding in mobile view

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -19,7 +19,7 @@
     @include('partials.analytics')
 </head>
 <body>
-    <nav class="mx-auto pt-8 px-8 flex items-center justify-between">
+    <nav class="mx-auto pt-8 pb-8 px-4 sm:px-8 flex items-center justify-between">
         <a class="flex items-center space-x-3 text-zinc-900 dark:text-white"
            href="/">
             <div class="inline-block h-6 w-6 bg-gradient-to-b from-pink-500 to-amber-500"></div>
