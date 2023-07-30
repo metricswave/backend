791889c51360d3419ef6f7a088a19a731efa6c3d
Víctor Falcón <victoor89@gmail.com>
2023-07-30 18:45:12 +0200

    Serve script from local path

--- resources/views/partials/analytics.blade.php
+++ resources/views/partials/analytics.blade.php
@@ -1,6 +1,6 @@
 <script defer
         event-uuid="fbe17995-b16b-45d5-b33e-7a43b9a41313"
-        src="https://tracker.metricswave.com/js/visits.js"></script>
+        src="/js/visits.js"></script>
 
 @if (config('app.env') === 'production')
     <script data-goatcounter="https://notifywave.goatcounter.com/count"
