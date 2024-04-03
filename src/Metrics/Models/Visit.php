97a886bb57f62fe05f2a7e0e8bb6f5e64493c770
Víctor Falcón <victoor89@gmail.com>
2024-04-03 21:28:35 +0100

    Set connection field on visits model

--- src/Metrics/Models/Visit.php
+++ src/Metrics/Models/Visit.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Visit extends Model
 {
+    protected $connection = 'visits';
+
     protected $guarded = [];
 
     protected $casts = ['list' => 'array', 'expired_at' => 'datetime'];
