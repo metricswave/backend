029ae9c2ec872fa087406de5856479e22da52f29
Víctor Falcón <victoor89@gmail.com>
2024-04-04 14:44:40 +0100

    With Cache feature Missing variable

--- src/Metrics/Infrastructure/helpers.php
+++ src/Metrics/Infrastructure/helpers.php
@@ -6,6 +6,6 @@ use MetricsWave\Metrics\MetricsInterface;
 if (! function_exists('visitsService')) {
     function visitsService($subject, $tag = 'visits', bool $withCache = true): MetricsInterface
     {
-        return (new MetricsBuilder())($subject, $tag);
+        return (new MetricsBuilder())($subject, $tag, $withCache);
     }
 }
