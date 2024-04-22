8b446992772dd368788168a54c327602be396b7d
Víctor Falcón <victoor89@gmail.com>
2024-04-22 12:09:43 +0200

    If team is already upgraded, change view

--- src/Pages/Http/Controllers/GetUpgradePageController.php
+++ src/Pages/Http/Controllers/GetUpgradePageController.php
@@ -3,14 +3,27 @@
 namespace MetricsWave\Pages\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Services\Plans\PlanGetter;
+use App\Transfers\PlanId;
 use Illuminate\View\View;
 use MetricsWave\Teams\Team;
 
 class GetUpgradePageController extends Controller
 {
+    public function __construct(private readonly PlanGetter $planGetter)
+    {
+    }
+
     public function __invoke(int $teamId): View
     {
         $team = Team::findOrFail($teamId);
+
+        if ($team->subscription_plan_id !== PlanId::FREE) {
+            return view('pages.team_upgraded', [
+                'team' => $team,
+                'plan' => $this->planGetter->get($team->subscription_plan_id)
+            ]);
+        }
 
         return view('pages.upgrade', [
             'team' => $team,
