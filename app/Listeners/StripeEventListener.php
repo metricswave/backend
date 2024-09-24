d59a9ec96355c8f52fd217ee568735b6ffc10429
Víctor Falcón <victoor89@gmail.com>
2024-09-24 15:06:15 +0200

    Unnecesary event

--- app/Listeners/StripeEventListener.php
+++ app/Listeners/StripeEventListener.php
@@ -2,12 +2,9 @@
 
 namespace App\Listeners;
 
-use App\Models\Lead;
-use App\Models\Price;
 use App\Services\Leads\FirstOrCreateLeadService;
 use Cache;
 use Laravel\Cashier\Events\WebhookReceived;
-use Log;
 use MetricsWave\Plan\Listeners\SubscribeTeamToPlanOnIntentSucceeded;
 
 class StripeEventListener
@@ -15,8 +12,7 @@ class StripeEventListener
     public function __construct(
         private readonly FirstOrCreateLeadService $firstOrCreateLeadService,
         private readonly SubscribeTeamToPlanOnIntentSucceeded $intentSucceeded,
-    ) {
-    }
+    ) {}
 
     public function handle(WebhookReceived $event): void
     {
@@ -26,33 +22,6 @@ class StripeEventListener
 
         Cache::put($event->payload['id'], $event->payload, now()->addMinutes(5));
 
-        if ($event->payload['type'] === 'checkout.session.completed') {
-            $leadUuid = $event->payload['data']['object']['metadata']['lead_uuid'];
-            $priceId = $event->payload['data']['object']['metadata']['price_id'];
-            $createLead = $event->payload['data']['object']['metadata']['create_lead'];
-
-            $price = Price::find($priceId);
-
-            if ($createLead === 'true') {
-                $email = $event->payload['data']['object']['customer_details']['email'];
-
-                $lead = ($this->firstOrCreateLeadService)($email);
-            } else {
-                $lead = Lead::where('uuid', $leadUuid)->first();
-            }
-
-            $lead->update([
-                'price_id' => $price->id,
-                'paid_price' => $price->price,
-                'paid_at' => now(),
-            ]);
-
-            $price->update([
-                'remaining' => max(0, $price->remaining - 1),
-            ]);
-            return;
-        }
-
         if ($event->payload['type'] === 'setup_intent.succeeded') {
             $teamId = (int) $event->payload['data']['object']['metadata']['team_id'];
             $planId = (int) $event->payload['data']['object']['metadata']['plan_id'];
@@ -60,6 +29,7 @@ class StripeEventListener
             $paymentMethodId = (string) $event->payload['data']['object']['payment_method'];
 
             $this->intentSucceeded->handle($teamId, $planId, $currency, $paymentMethodId);
+
             return;
         }
     }
