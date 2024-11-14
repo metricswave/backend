a8415943898d63e8881072d0f55a7b800f52b608
Víctor Falcón <victoor89@gmail.com>
2024-11-14 19:15:04 +0000

    feat(app/Console/Kernel.php): Add email notification to yearly visits creation command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,8 @@ class Kernel extends ConsoleKernel
         $schedule->command('app:teams:mail-without-events --sub-days=1')->twiceDaily(10, 18);
 
         // Create next year vistis table
-        $schedule->command('visits:create-table --execute')->yearlyOn(12, 1, '06:00');
+        $schedule->command('visits:create-table --execute')->yearlyOn(12, 1, '06:00')
+            ->emailOutputTo('victoor89@gmail.com');
 
         // Telegram Bot
         $schedule->command('telegram:reply-with-group-id')->everyTwoSeconds();
