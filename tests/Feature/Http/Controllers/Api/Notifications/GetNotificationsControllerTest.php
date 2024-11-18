89807f6163d1aadf85569416ccce59bd4181ddc4
Víctor Falcón <victoor89@gmail.com>
2024-11-18 11:59:14 +0000

    Refactor GetNotificationsControllerTest

--- tests/Feature/Http/Controllers/Api/Notifications/GetNotificationsControllerTest.php
+++ tests/Feature/Http/Controllers/Api/Notifications/GetNotificationsControllerTest.php
@@ -2,21 +2,29 @@
 
 use App\Models\Trigger;
 use App\Models\TriggerType;
+use App\Models\User;
 use App\Notifications\TriggerNotification;
 use Illuminate\Support\Carbon;
+use MetricsWave\Teams\Team;
 
 use function Pest\Laravel\actingAs;
 
-it('return a expected list of user notifications', function () {
+$sendNotifications = function (User $user, Team $team) {
+    $triggerType = TriggerType::factory()->create();
+
+    foreach (Trigger::factory()->for($team)->for($triggerType)->count(3)->create() as $k => $trigger) {
+        $user->notify(new TriggerNotification($trigger, [
+            'user' => 'user_'.$k,
+        ]));
+    }
+};
+
+it('return a expected list of user notifications', function () use ($sendNotifications) {
     Carbon::setTestNow(now());
     DB::connection('visits')->table('notifications_'.now()->year)->truncate();
 
     [$user, $team] = user_with_team();
-    $triggerType = TriggerType::factory()->create();
-
-    foreach (Trigger::factory()->for($team)->for($triggerType)->count(3)->create() as $trigger) {
-        $user->notify(new TriggerNotification($trigger));
-    }
+    $sendNotifications($user, $team);
 
     actingAs($user)
         ->getJson('/api/teams/'.$team->id.'/notifications')
@@ -31,18 +39,12 @@ it('return a expected list of user notifications', function () {
         ]);
 });
 
-it('return a expected list of user notifications while filtering by user_parameter', function () {
+it('return a expected list of user notifications while filtering by user_parameter', function () use ($sendNotifications) {
     Carbon::setTestNow(now());
-    \DB::connection('visits')->table('notifications_'.now()->year)->truncate();
+    DB::connection('visits')->table('notifications_'.now()->year)->truncate();
 
     [$user, $team] = user_with_team();
-    $triggerType = TriggerType::factory()->create();
-
-    foreach (Trigger::factory()->for($team)->for($triggerType)->count(3)->create() as $k => $trigger) {
-        $user->notify(new TriggerNotification($trigger, [
-            'user' => 'user_'.$k,
-        ]));
-    }
+    $sendNotifications($user, $team);
 
     actingAs($user)
         ->getJson('/api/teams/'.$team->id.'/notifications?user_parameter=user_1')
