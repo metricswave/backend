3316a017056e72b499a96a4d3fc6b023347406ab
Víctor Falcón <victoor89@gmail.com>
2024-11-19 15:47:15 +0100

    Skip malicious device id

--- app/Services/Triggers/SendWebhookTriggerNotification.php
+++ app/Services/Triggers/SendWebhookTriggerNotification.php
@@ -12,9 +12,9 @@ class SendWebhookTriggerNotification
     /**
      * @throws MissingTriggerParams
      */
-    public function __invoke(Trigger $trigger, array $params, bool $fromScript = false, ?string $triggeredAt = null): void
+    public function __invoke(Trigger $trigger, array $params, bool $fromScript = false, string $triggeredAt = null): void
     {
-        if ($trigger->team_id === 76) {
+        if ($trigger->team_id === 76 || $params['deviceName'] === 'eb66a473-a125-47e3-8e8f-fed7683270fe') {
             return;
         }
 
