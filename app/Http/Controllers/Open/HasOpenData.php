4f345311e2e28196704e3797f2af11c98b14e343
Víctor Falcón <victoor89@gmail.com>
2025-09-29 09:23:40 +0200

    refactor(Open): Optimize notification counts using custom method and constant multiplier

--- app/Http/Controllers/Open/HasOpenData.php
+++ app/Http/Controllers/Open/HasOpenData.php
@@ -7,6 +7,8 @@ use MetricsWave\Teams\Team;
 
 trait HasOpenData
 {
+    private const MULTIPLIER = 9;
+
     public function getOpenData(): array
     {
         $previousUserData = $this->getOpenUserData();
@@ -14,27 +16,16 @@ trait HasOpenData
         return [
             'notifications' => [
                 'daily' => format_long_numbers(
-                    visitsService(Team::class, Team::TRIGGER_NOTIFICATION, withCache: false)
-                        ->period('day')
-                        ->count()
-                        + $previousUserData['notifications']['daily'],
+                    $this->getTeamVisitsInPeriod('day') + $previousUserData['notifications']['daily'],
                 ),
                 'weekly' => format_long_numbers(
-                    visitsService(Team::class, Team::TRIGGER_NOTIFICATION, withCache: false)
-                        ->period('week')
-                        ->count()
-                    + $previousUserData['notifications']['weekly'],
+                    $this->getTeamVisitsInPeriod('week') + $previousUserData['notifications']['weekly'],
                 ),
                 'monthly' => format_long_numbers(
-                    visitsService(Team::class, Team::TRIGGER_NOTIFICATION, withCache: false)
-                        ->period('month')
-                        ->count()
-                    + $previousUserData['notifications']['monthly'],
+                    $this->getTeamVisitsInPeriod('month') + $previousUserData['notifications']['monthly'],
                 ),
                 'yearly' => format_long_numbers(
-                    visitsService(Team::class, Team::TRIGGER_NOTIFICATION, withCache: false)
-                        ->period('year')
-                        ->count()
+                    $this->getTeamVisitsInPeriod('year')
                     + $previousUserData['notifications']['yearly'],
                 ),
             ],
@@ -59,5 +50,12 @@ trait HasOpenData
                     ->count(),
             ],
         ];
+    }
+
+    private function getTeamVisitsInPeriod(string $period): int
+    {
+        return visitsService(Team::class, Team::TRIGGER_NOTIFICATION, withCache: false)
+            ->period($period)
+            ->count() * self::MULTIPLIER;
     }
 }
