b18626b60c64140525149adab45234c078b5e544
Víctor Falcón <victoor89@gmail.com>
2025-10-04 21:50:53 +0200

    Fix broken tests

--- tests/Feature/Http/Controllers/Api/Dashboard/GetDashboardsControllerTest.php
+++ tests/Feature/Http/Controllers/Api/Dashboard/GetDashboardsControllerTest.php
@@ -11,8 +11,7 @@ it('return default dashboard if user has not any', function () {
         ->assertJson(fn (AssertableJson $json) => $json
             ->count('data', 1)
             ->has('data.0', fn (AssertableJson $json) => $json
-                ->where('id', 1)
-                ->where('team_id', 1)
+                ->where('team_id', $team->id)
                 ->where('name', 'Default')
                 ->where('items', [])
                 ->etc()
